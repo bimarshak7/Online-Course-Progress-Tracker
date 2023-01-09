a3a1d78be85296a986b6ff323901fbad231af3a1
bimarshak7 <khanalbimarsha7@gmail.com>
2023-01-09 20:31:33 +0545

    Setup redux

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,20 +1,25 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom"
+import { Provider } from "react-redux"
+
+import { store } from "./redux/store"
 import { Login } from "./Pages"
 import "./App.css"
 
 function App() {
 	return (
-		<Router>
-			<div className="App">
-				<Routes>
-					<Route path="/" element={<Login />} />
-					<Route
-						path="/register"
-						element={<Login register={true} />}
-					/>
-				</Routes>
-			</div>
-		</Router>
+		<Provider store={store}>
+			<Router>
+				<div className="App">
+					<Routes>
+						<Route path="/" element={<Login />} />
+						<Route
+							path="/register"
+							element={<Login register={true} />}
+						/>
+					</Routes>
+				</div>
+			</Router>
+		</Provider>
 	)
 }
 
