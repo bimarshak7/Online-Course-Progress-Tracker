5b17d01bdee09b77b10039a8f4c3c3570f05ed83
bimarshak7 <khanalbimarsha7@gmail.com>
2023-01-16 19:26:43 +0545

    Changes in db schema

--- server/db/schema.sql
+++ server/db/schema.sql
@@ -1,3 +1,4 @@
+DROP TABLE IF EXISTS chapters;
 DROP TABLE IF EXISTS courses;
 DROP TABLE IF EXISTS users;
 
@@ -9,18 +10,33 @@ CREATE TABLE IF NOT EXISTS users (
   password       VARCHAR(60) NOT NULL,
   grad_year      INT,
   path           VARCHAR(50),
-  
+  public         BOOLEAN DEFAULT 0,
   PRIMARY KEY(puid,id,email)
 );
 
 CREATE TABLE IF NOT EXISTS courses (
   id             SERIAL,
-  pcid           VARCHAR(36) DEFAULT (UUID()),
+  pcid           VARCHAR(36) NOT NULL DEFAULT (UUID()),
   name           VARCHAR(50) NOT NULL,
   chapters       INT DEFAULT 1,
   category       VARCHAR(50),
+  added_on       DATE DEFAULT (CURDATE()),
+  completed      BOOLEAN DEFAULT 0,
   puid           VARCHAR(36),
   
-  PRIMARY KEY(id,pcid),
+  PRIMARY KEY(pcid,id),
   FOREIGN KEY (puid) REFERENCES users(puid) ON DELETE CASCADE
+);
+
+CREATE TABLE IF NOT EXISTS chapters (
+  id             SERIAL,
+  pchid           VARCHAR(36) DEFAULT (UUID()),
+  title           VARCHAR(50) NOT NULL,
+  remarks       VARCHAR(50) DEFAULT '',
+  completed      BOOLEAN DEFAULT 0,
+  date_completed  DATE,
+  pcid           VARCHAR(36),
+  
+  PRIMARY KEY(id,pcid),
+  FOREIGN KEY (pcid) REFERENCES courses(pcid) ON DELETE CASCADE
 );
